e61d3d266797821b0bf916d03272823546247cc7
HACHI3232 <harada.chisato526@gmail.com>
2023-03-19 22:05:42 +0900

    sinki-touroku-gamenikou

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
+  resources :prototypes, only: [:index, :new, :create]do
+  
+  end
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
